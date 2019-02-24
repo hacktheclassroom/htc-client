88344b231b8909d324c54108adef12746909af0f
vesche <austin.jackson@swimlane.com>
2019-02-24 02:45:07 -0700

    working on phishing game

--- htc_client/minigames/Phishing.py
+++ htc_client/minigames/Phishing.py
@@ -5,6 +5,9 @@ import sys
 import pygame
 from pygame.locals import *
 
+sys.path.append('..')
+from common import Background
+
 NAME = 'Phishing'
 POINTS = 300
 IMG = './minigames/img/phishing.png'
@@ -12,8 +15,9 @@ IMG = './minigames/img/phishing.png'
 
 def main(player, surface, font, clock):
 
-    foo = font.render('Phishing minigame', False, (0, 0, 0))
+    foo = font.render('Is this a Phishing email?', False, (0, 0, 0))
     button = pygame.Rect(100, 100, 100, 100)
+    bg = Background('./minigames/img/phishing_background.jpg', [0, 0])
 
     while True:
         clock.tick(60)
@@ -30,7 +34,8 @@ def main(player, surface, font, clock):
                     player.client.solve(NAME, flag)
                     return
 
-        surface.fill((233, 233, 233))
+        surface.fill([255, 255, 255])
+        surface.blit(bg.image, bg.rect)
         surface.blit(foo, (10, 10))
         pygame.draw.rect(surface, [212, 0, 0], button)
 
